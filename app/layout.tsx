31cf52ad2033044821e44978996217f5d03e4469
amith-vp <amithvp002@gmail.com>
2024-11-28 19:43:02 +0530

    clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,15 @@ export default function RootLayout({
             gtag('config', 'G-YZPTFZNCLP');
           `}
         </Script>
+        <Script id="microsoft-clarity-analytics">
+  {`
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "p5nr5sdd1z");
+  `}
+</Script>
       </head>
       <body className="font-jetbrains">
         <ThemeProvider
